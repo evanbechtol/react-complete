0971aeb1405850c260932acc8a679d356d515335
evan bechtol <evanbechtol@gmail.com>
2021-04-21 13:05:10 -0500

    Utilizing State
    - Add state to App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      {name: "Evan", age: 31},
+      {name: "Morgan", age: 30},
+      {name: "Taylor", age: 29},
+      {name: "Alex", age: 26}
+    ]
+  };
+
+  render() {
+    return(
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My hobbies are: Programming, and video games</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <Person name={this.state.persons[3].name} age={this.state.persons[3].age}/>
+      </div>
+    );
+  }
 }
 
 export default App;
