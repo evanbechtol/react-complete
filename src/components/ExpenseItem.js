e61fab6f27ccdfc141bfedd8a2eb85fd302112de
Evan Bechtol <evanbechtol@gmail.com>
2021-05-03 16:47:44 -0500

    Dynamically passing data via props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,13 @@
-import "./ExpenseItem.css";
+import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-    const {date, expense, amount} = props;
-
+    const {date, title, amount} = props;
     return (
-        <div className="expense-item">
-            <div>{date}</div>
-            <div className="expense-item__description">
-                <h2>{expense}</h2>
-                <div className="expense-item__price">{amount}</div>
+        <div className='expense-item'>
+            <div>{date.toISOString()}</div>
+            <div className='expense-item__description'>
+                <h2>{title}</h2>
+                <div className='expense-item__price'>${amount}</div>
             </div>
         </div>
     );
