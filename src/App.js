dbf557a9966a8f4620a7662b0c967bd382fbd730
Evan Bechtol <evanbechtol@gmail.com>
2021-05-03 16:08:57 -0500

    Use ExpenseItem in App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import './App.css';
-import Person from './Person/Person';
+import ExpenseItem from "./components/ExpenseItem";
 
 class App extends Component {
-  state = {
-    persons: [
-      {name: "Evan", age: 31},
-      {name: "Morgan", age: 30},
-      {name: "Taylor", age: 29},
-      {name: "Alex", age: 26}
-    ]
-  };
-
-  render() {
-    return(
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <button>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My hobbies are: Programming, and video games</Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-        <Person name={this.state.persons[3].name} age={this.state.persons[3].age}/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <h1>Hi, I'm a React App</h1>
+                <ExpenseItem/>
+            </div>
+        );
+    }
 }
 
 export default App;
