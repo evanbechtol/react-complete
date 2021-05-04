4aad0ab079ee9a8e99d18c5c2d8258f22f88d139
Evan Bechtol <evanbechtol@gmail.com>
2021-05-04 08:03:39 -0500

    Fix rendering issue with ExpenseDate

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,8 +1,8 @@
 function ExpenseDate(props) {
     const {date} = props;
-    const month = date.toLocaleString('en-US', { month: 'long' });
-    const day = date.toLocaleString('en-US',{ day: '2-digit' });
-    const year= date.getFullYear();
+    const month = date.toLocaleString('en-US', {month: 'long'});
+    const day = date.toLocaleString('en-US', {day: '2-digit'});
+    const year = date.getFullYear();
 
     return (
         <div>
