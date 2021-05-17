b7b4f7b0dba312e4369c31ea4e8f3fc05361972e
evan bechtol <evanbechtol@gmail.com>
2021-05-17 10:17:13 -0500

    Lecture 59: Lifting the state "up"
    - Learn to pass pointers  of functions to children components, which allow us to pass the state up to the parent

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,25 +1,37 @@
-import ExpenseItem from "./ExpenseItem";
+import React, { useState } from "react";
+
 import "./Expenses.css";
 import Card from "../UI/Card";
+import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "../ExpenseFilter/ExpenseFilter";
 
 const Expenses = ( props ) => {
   const { expenses } = props;
+  const [ selectedYear, setSelectedYear ] = useState( "" );
+
+  const filterChangedHandler = (updatedFilter) => {
+    setSelectedYear(updatedFilter);
+    console.log('State updated', updatedFilter);
+  };
 
   return (
-    <Card className="expenses">
-      <ExpenseItem title={expenses[ 0 ].title}
-                   amount={expenses[ 0 ].amount}
-                   date={expenses[ 0 ].date}
-                   />
-      <ExpenseItem title={expenses[ 1 ].title}
-                   amount={expenses[ 1 ].amount}
-                   date={expenses[ 1 ].date}
-                   />
-      <ExpenseItem title={expenses[ 2 ].title}
-                   amount={expenses[ 2 ].amount}
-                   date={expenses[ 2 ].date}
-                   />
-    </Card>
+    <div>
+      <ExpensesFilter onFilterChange={filterChangedHandler}/>
+      <Card className="expenses">
+        <ExpenseItem title={expenses[ 0 ].title}
+                     amount={expenses[ 0 ].amount}
+                     date={expenses[ 0 ].date}
+        />
+        <ExpenseItem title={expenses[ 1 ].title}
+                     amount={expenses[ 1 ].amount}
+                     date={expenses[ 1 ].date}
+        />
+        <ExpenseItem title={expenses[ 2 ].title}
+                     amount={expenses[ 2 ].amount}
+                     date={expenses[ 2 ].date}
+        />
+      </Card>
+    </div>
   );
 };
 
