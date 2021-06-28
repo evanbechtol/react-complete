f1d352a1b929311b8a87a442b83890fd85f89e40
evan bechtol <evanbechtol@gmail.com>
2021-06-28 09:19:35 -0500

    Lecture 67: Conditional Return Statements
    - Refactor `ExpensesList.js` to simplify overall implementation

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -1,21 +1,24 @@
 import React from "react";
 import ExpenseItem from "./ExpenseItem";
+import "./ExpensesList.css";
 
-const ExpensesList = ({items}) => {
-  let expensesContent = <p>No expenses found.</p>;
-
-  if (items.length > 0) {
-    expensesContent = items.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
+const ExpensesList = ( { items } ) => {
+  if ( items.length === 0 ) {
+    return <h2 className="expenses-list__fallback">No expenses found.</h2>;
   }
 
-  return expensesContent;
-}
+  return <ul className="expenses-list">
+    {
+      items.map( ( expense ) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ) )
+    }
+  </ul>;
+};
 
 export default ExpensesList;
